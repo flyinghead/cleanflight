adb6e87e5fdac1f9f03ba28a8ed31e1d7088e063
Flyinghead <raphael.jean@gmail.com>
2016-03-01 21:48:40 +0100

    Save raw throttle value to compare using alt_hold_deadband (as proposed in upstream PR) Substract alt_hold_deadband in slow alt change mode for super smooth start and stop.

--- src/main/flight/altitudehold.c
+++ src/main/flight/altitudehold.c
@@ -77,6 +77,7 @@ void configureAltitudeHold(
 
 #if defined(BARO) || defined(SONAR)
 
+static int16_t initialRawThrottleHold;
 static int16_t initialThrottleHold;
 static int32_t EstAlt;                // in cm
 
@@ -91,10 +92,10 @@ static void applyMultirotorAltHold(void)
     // multirotor alt hold
     if (rcControlsConfig->alt_hold_fast_change) {
         // rapid alt changes
-        if (ABS(rcCommand[THROTTLE] - initialThrottleHold) > rcControlsConfig->alt_hold_deadband) {
+        if (ABS(rcData[THROTTLE] - initialRawThrottleHold) > rcControlsConfig->alt_hold_deadband) {
             errorVelocityI = 0;
             isAltHoldChanged = 1;
-            rcCommand[THROTTLE] += (rcCommand[THROTTLE] > initialThrottleHold) ? -rcControlsConfig->alt_hold_deadband : rcControlsConfig->alt_hold_deadband;
+            rcCommand[THROTTLE] += (rcData[THROTTLE] > initialRawThrottleHold) ? -rcControlsConfig->alt_hold_deadband : rcControlsConfig->alt_hold_deadband;
         } else {
             if (isAltHoldChanged) {
                 AltHold = EstAlt;
@@ -104,9 +105,11 @@ static void applyMultirotorAltHold(void)
         }
     } else {
         // slow alt changes, mostly used for aerial photography
-        if (ABS(rcCommand[THROTTLE] - initialThrottleHold) > rcControlsConfig->alt_hold_deadband) {
+        if (ABS(rcData[THROTTLE] - initialRawThrottleHold) > rcControlsConfig->alt_hold_deadband) {
             // set velocity proportional to stick movement +100 throttle gives ~ +50 cm/s
-            setVelocity = (rcCommand[THROTTLE] - initialThrottleHold) / 2;
+            setVelocity = rcData[THROTTLE] - initialRawThrottleHold;
+            setVelocity += setVelocity > 0 ? -rcControlsConfig->alt_hold_deadband : rcControlsConfig->alt_hold_deadband;
+            setVelocity /= 2;
             velocityControl = 1;
             isAltHoldChanged = 1;
         } else if (isAltHoldChanged) {
@@ -147,6 +150,7 @@ void updateAltHoldState(void)
     if (!FLIGHT_MODE(BARO_MODE)) {
         ENABLE_FLIGHT_MODE(BARO_MODE);
         AltHold = EstAlt;
+        initialRawThrottleHold = rcData[THROTTLE];
         initialThrottleHold = rcCommand[THROTTLE];
         errorVelocityI = 0;
         altHoldThrottleAdjustment = 0;
@@ -164,6 +168,7 @@ void updateSonarAltHoldState(void)
     if (!FLIGHT_MODE(SONAR_MODE)) {
         ENABLE_FLIGHT_MODE(SONAR_MODE);
         AltHold = EstAlt;
+        initialRawThrottleHold = rcData[THROTTLE];
         initialThrottleHold = rcCommand[THROTTLE];
         errorVelocityI = 0;
         altHoldThrottleAdjustment = 0;
